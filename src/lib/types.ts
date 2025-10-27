3d6c201e266816deb6565bb75340a1566f8bef9a
Clionegohan <sawakiguchik@gmail.com>
2025-10-27 22:09:00 +0900

    fix/typo

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,6 +2,6 @@ export type Illustration = {
   readonly id: string;
   readonly slug: string;
   readonly name: string;
-  readonly descriptign: string;
-  readonly imageurl: string;
+  readonly description: string;
+  readonly imageUrl: string;
 };
