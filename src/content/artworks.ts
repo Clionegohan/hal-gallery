c76c1f63d506173da79ff8e5bb82895017da6011
Clionegohan <sawakiguchik@gmail.com>
2025-10-30 20:04:27 +0900

    dummy-dataの追加

---
+++ src/content/artworks.ts
@@ -0,0 +1,19 @@
+import type { Artwork } from "@/lib/types";
+
+export const ARTWORKS: Artwork[] = [
+  {
+    id: "001",
+    slug: "dammy-001",
+    name: "dummyでーす",
+    description: "dummyなんですよね",
+    imageUrl: "/illustrations/dummy-001.png",
+  },
+  {
+    id: "dammy-002",
+    slug: "dammy-002",
+    name: "dummyなのです",
+    description: "dummyでござ",
+    imageUrl: "/illustrations/dummy-002.png",
+  },
+];
+
