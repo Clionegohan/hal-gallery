88a06eab62be0f24b5cb5f8c5d72d60c9d855d00
Clionegohan <sawakiguchik@gmail.com>
2025-11-05 22:58:18 +0900

    デザインの微小修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,29 +3,26 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "hal-gallery",
-  description: "halが好きな絵師さんにskebにて依頼したイラストを自慢するだけのサイトです",
+  description:
+    "halが好きな絵師さんにskebにて依頼したイラストを自慢するだけのサイトです",
 };
 
 export default function RootLayout({
   children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
   return (
-  <html lang="ja">
-    <body className="mx-auto max-w-5xl bg-gray-50 text-gray-900">
-      <header className="border-b bg-white px-4 py-3 font-semibold">
-        <div>
-          hal gallery
-        </div>
-      </header>
-      <main className="px-4 py-8">{children}</main>
-      <footer className="border-t bg-white px-4 py-6 text-gray-500">
-        <div>
-          © {new Date().getFullYear()} hal gallery
-        </div>
-      </footer>
-    </body>
-  </html>
+    <html lang="ja">
+      <body className="mx-auto max-w-5xl bg-black text-white">
+        <header className="border-b bg-black px-4 py-3 font-semibold">
+          <div>hal gallery</div>
+        </header>
+        <main className="px-4 py-8">{children}</main>
+        <footer className="border-t bg-black px-4 py-6 text-gray-400">
+          <div>© {new Date().getFullYear()} hal gallery</div>
+        </footer>
+      </body>
+    </html>
   );
 }
