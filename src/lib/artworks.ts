b446f88f285b7f72a5897421501fd8ded22cfc32
Clionegohan <sawakiguchik@gmail.com>
2025-11-06 15:00:09 +0900

    slugによってイラストを取得するメソッドの実装

--- src/lib/artworks.ts
+++ src/lib/artworks.ts
@@ -2,3 +2,7 @@ import type { Artwork } from "@/lib/types";
 import { ARTWORKS } from "@/content/artworks";
 
 export const getAllArtworks = (): readonly Artwork[] => ARTWORKS;
+
+export const getArtworksBySlug = (slug: string): Artwork | undefined => {
+  return ARTWORKS.find((artwork) => artwork.slug === slug);
+};
